594e562aa914937200065dcf45ceee3537ff8ef5
lateral404 <990667303@qq.com>
2022-01-12 16:58:21 +0800

    HelloWorld

---
+++ HelloWorld/Demo1.java
@@ -0,0 +1,20 @@
+//使用System.out.pintln("打印诗句")
+
+/**
+ * git init
+ * git add .
+ * git commit -m "first commit"
+ * git branch -M main
+ * git remote add origin git@github.com:Cat00011cat/javaDemo.git
+ * git push -u origin main
+ * **/
+
+
+public class Demo1{
+    public static void main(String[] ages){
+        System.out.println("只有停止抱怨，");
+        System.out.println("才能集中精力解决问题，");
+        System.out.println("唯有多加努力，");
+        System.out.println("才有赢的可能。");
+    }
+}
